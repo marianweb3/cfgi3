bde863573bee99890783f8b804fd9d9d186a95a8
liutsar <liutsar346@gmail.com>
2024-07-16 16:08:22 +0300

    Configure eslintrc.cjs and .gitignore

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+};
